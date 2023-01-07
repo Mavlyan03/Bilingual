d62b21c562e0d838e635e7b79c5280dd85bb7a9f
User <mavlyan.sadirov@gmail.com>
2023-01-07 13:29:10 +0600

    Entities

--- src/main/java/com/example/bilingual/db/model/Option.java
+++ src/main/java/com/example/bilingual/db/model/Option.java
@@ -1,4 +1,19 @@
 package com.example.bilingual.db.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "options")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Option {
+    @Id
+    @SequenceGenerator(name = "option_seq",sequenceName = "option_seq",allocationSize = 1, initialValue = 10)
+    @GeneratedValue(generator = "option_seq",strategy = GenerationType.SEQUENCE)
+    private Long id;
 }
--- src/main/java/com/example/bilingual/db/model/Test.java
+++ src/main/java/com/example/bilingual/db/model/Test.java
@@ -1,4 +1,19 @@
 package com.example.bilingual.db.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tests")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Test {
+    @Id
+    @SequenceGenerator(name = "test_seq",sequenceName = "test_seq",allocationSize = 1, initialValue = 10)
+    @GeneratedValue(generator = "test_seq",strategy = GenerationType.SEQUENCE)
+    private Long id;
 }
