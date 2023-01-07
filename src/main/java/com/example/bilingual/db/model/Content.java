4043f4c8034f838b97305d1f6263f8860180c2a2
User <mavlyan.sadirov@gmail.com>
2023-01-07 18:39:41 +0600

    Relationships

--- src/main/java/com/example/bilingual/db/model/Content.java
+++ src/main/java/com/example/bilingual/db/model/Content.java
@@ -1,5 +1,6 @@
 package com.example.bilingual.db.model;
 
+import com.example.bilingual.db.model.enums.ContentFormat;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,4 +17,8 @@ public class Content {
     @SequenceGenerator(name = "content_seq",sequenceName = "content_seq",allocationSize = 1, initialValue = 10)
     @GeneratedValue(generator = "content_seq",strategy = GenerationType.SEQUENCE)
     private Long id;
+
+    private String content;
+    @Enumerated(EnumType.STRING)
+    private ContentFormat contentFormat;
 }
