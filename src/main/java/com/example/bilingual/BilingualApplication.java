76138a13299391b4fee2572593b3aa1bdeab5bd3
User <mavlyan.sadirov@gmail.com>
2023-01-07 12:00:02 +0600

    Bilingual Project

---
+++ src/main/java/com/example/bilingual/BilingualApplication.java
@@ -0,0 +1,13 @@
+package com.example.bilingual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilingualApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilingualApplication.class, args);
+    }
+
+}
