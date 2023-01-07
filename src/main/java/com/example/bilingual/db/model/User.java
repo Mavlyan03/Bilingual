5e5be0d949a814588809d6b36a18d6fd6c164c0a
User <mavlyan.sadirov@gmail.com>
2023-01-07 13:22:08 +0600

    Entities

--- src/main/java/com/example/bilingual/db/model/User.java
+++ src/main/java/com/example/bilingual/db/model/User.java
@@ -1,4 +1,25 @@
 package com.example.bilingual.db.model;
 
+
+import com.example.bilingual.db.enums.Role;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
+    @Id
+    @SequenceGenerator(name = "user_seq", sequenceName = "user_seq", allocationSize = 1, initialValue = 10)
+    @GeneratedValue(generator = "user_seq", strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
