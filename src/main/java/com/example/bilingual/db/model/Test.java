f22865fcc6072f723f9891fa5119e2f8d55b2c8c
User <mavlyan.sadirov@gmail.com>
2023-01-07 18:57:35 +0600

    Relationships

--- src/main/java/com/example/bilingual/db/model/Test.java
+++ src/main/java/com/example/bilingual/db/model/Test.java
@@ -5,6 +5,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
+
+import static javax.persistence.CascadeType.ALL;
 
 @Entity
 @Table(name = "tests")
@@ -16,4 +19,9 @@ public class Test {
     @SequenceGenerator(name = "test_seq",sequenceName = "test_seq",allocationSize = 1, initialValue = 10)
     @GeneratedValue(generator = "test_seq",strategy = GenerationType.SEQUENCE)
     private Long id;
+    private String title;
+    private String description;
+
+    @OneToMany(cascade = ALL, mappedBy = "test")
+    private List<Question> questions;
 }
