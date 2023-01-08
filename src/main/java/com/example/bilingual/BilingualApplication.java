a20f7cba238255295fbae21cd449e08687ea4006
User <mavlyan.sadirov@gmail.com>
2023-01-08 19:43:25 +0600

    Relationships

--- src/main/java/com/example/bilingual/BilingualApplication.java
+++ src/main/java/com/example/bilingual/BilingualApplication.java
@@ -2,12 +2,19 @@ package com.example.bilingual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 @SpringBootApplication
 public class BilingualApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BilingualApplication.class, args);
+        System.out.println("Welcome colleagues, project name is Bilingual!");
     }
 
+    @GetMapping("/")
+    public String greetingPage(){
+        return "welcome";
+    }
 }
