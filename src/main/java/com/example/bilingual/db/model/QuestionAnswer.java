a552f78e67b17196de831dee2c106639a676b30a
User <mavlyan.sadirov@gmail.com>
2023-01-08 19:32:36 +0600

    Relationships

--- src/main/java/com/example/bilingual/db/model/QuestionAnswer.java
+++ src/main/java/com/example/bilingual/db/model/QuestionAnswer.java
@@ -1,10 +1,15 @@
 package com.example.bilingual.db.model;
 
+import com.example.bilingual.db.model.enums.Status;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+
+import java.util.Set;
+
+import static javax.persistence.CascadeType.*;
 
 @Entity
 @Table(name = "question_answers")
@@ -16,4 +21,42 @@ public class QuestionAnswer {
     @SequenceGenerator(name = "answer_seq",sequenceName = "answer_seq",allocationSize = 1, initialValue = 10)
     @GeneratedValue(generator = "answer_seq",strategy = GenerationType.SEQUENCE)
     private Long id;
+
+    private Integer numberOfWords;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    private Boolean seen;
+
+    private Integer countOfPlays;
+
+    private Float score;
+
+    private String textUserResponse;
+
+    @OneToOne(cascade = {
+            REFRESH,
+            MERGE,
+            DETACH})
+    private Question question;
+
+    @OneToOne(cascade = {
+            REFRESH,
+            MERGE,
+            DETACH
+    })
+    private Content content;
+
+    @ManyToMany(cascade = {
+            REFRESH,
+            MERGE,
+            DETACH})
+    private Set<Option> options;
+
+    @ManyToOne(cascade = {
+            REFRESH,
+            MERGE,
+            DETACH})
+    private Result result;
 }
