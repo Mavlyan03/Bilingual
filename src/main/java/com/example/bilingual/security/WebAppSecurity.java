7389791bb10760e1c22001cd2b03762200355fad
User <mavlyan.sadirov@gmail.com>
2023-01-09 21:41:36 +0600

    Security

--- src/main/java/com/example/bilingual/security/WebAppSecurity.java
+++ src/main/java/com/example/bilingual/security/WebAppSecurity.java
@@ -1,9 +1,41 @@
 package com.example.bilingual.security;
 
+import com.example.bilingual.security.jwt.JwtTokenVerifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableGlobalMethodSecurity(
         prePostEnabled = true
 )
 public class WebAppSecurity {
+
+    private final JwtTokenVerifier jwtTokenVerifier;
+
+    public WebAppSecurity(JwtTokenVerifier jwtTokenVerifier) {
+        this.jwtTokenVerifier = jwtTokenVerifier;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors()
+                .and()
+                .csrf().disable()
+                .authorizeRequests(
+                        (auth) -> auth.antMatchers("/swagger",
+                                        "v3/api-docs/**",
+                                        "/swagger-ui/index.html").permitAll()
+                                .anyRequest().permitAll());
+        http.addFilterBefore(jwtTokenVerifier, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/com/example/bilingual/security/jwt/JwtTokenVerifier.java
+++ src/main/java/com/example/bilingual/security/jwt/JwtTokenVerifier.java
@@ -1,4 +1,65 @@
 package com.example.bilingual.security.jwt;
 
-public class JwtTokenVerifier {
+import com.example.bilingual.db.model.User;
+import com.example.bilingual.db.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenVerifier extends OncePerRequestFilter {
+
+    private final JwtTokenUtil jwtTokenUtil;
+    private final UserRepository userRepository;
+
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String BEARER = "Bearer ";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        Optional<String> optional = getJwtFromRequest(request);
+
+        optional.ifPresent(token -> {
+                    String email = jwtTokenUtil.validateJWTToken(token);
+
+                    User user = userRepository.findByEmail(email).orElseThrow(
+                            () -> new UsernameNotFoundException(String.format("email(%s) not found", email)));
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                            user,
+                            null,
+                            user.getAuthorities()
+                    );
+
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+                }
+        );
+
+        filterChain.doFilter(request, response);
+    }
+
+    private Optional<String> getJwtFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORIZATION);
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER)) {
+            return Optional.of(bearerToken.substring(BEARER.length()));
+        }
+
+        return Optional.empty();
+    }
 }
