4c22846667f4429dd1255879dcf2a97226d32a00
User <mavlyan.sadirov@gmail.com>
2023-01-10 11:48:15 +0600

    Register

---
+++ src/main/java/com/example/bilingual/exception/GlobalException.java
@@ -0,0 +1,28 @@
+package com.example.bilingual.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GlobalException {
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ExceptionResponse notFound(NotFoundException ex) {
+        return new ExceptionResponse(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionResponse badRequest(BadRequestException ex) {
+        return new ExceptionResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ExceptionResponse badCredentials(BadCredentialsException ex) {
+        return new ExceptionResponse(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+}
