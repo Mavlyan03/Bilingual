41e7f6070372a73b6ec04901c69d77cde01ccff5
User <mavlyan.sadirov@gmail.com>
2023-01-10 11:28:17 +0600

    Register

--- src/main/java/com/example/bilingual/dto/request/RegisterRequest.java
+++ src/main/java/com/example/bilingual/dto/request/RegisterRequest.java
@@ -3,11 +3,15 @@ package com.example.bilingual.dto.request;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
+@NotBlank
 public class RegisterRequest {
     private String firstName;
     private String lastName;
     private String email;
+    @NotBlank
     private String password;
 }
