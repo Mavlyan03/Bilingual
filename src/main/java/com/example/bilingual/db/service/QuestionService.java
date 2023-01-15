eff536fc4aecce86acf022fb8610b09ca56299ad
User <mavlyan.sadirov@gmail.com>
2023-01-15 17:58:23 +0600

    Question service

--- src/main/java/com/example/bilingual/db/service/QuestionService.java
+++ src/main/java/com/example/bilingual/db/service/QuestionService.java
@@ -4,6 +4,7 @@ import com.example.bilingual.db.model.Option;
 import com.example.bilingual.db.model.Question;
 import com.example.bilingual.db.model.Test;
 import com.example.bilingual.db.model.enums.ContentFormat;
+import com.example.bilingual.db.model.enums.OptionType;
 import com.example.bilingual.db.model.enums.QuestionType;
 import com.example.bilingual.db.repository.QuestionRepository;
 import com.example.bilingual.db.repository.TestRepository;
@@ -27,10 +28,10 @@ public class QuestionService {
         Test test = testRepository.findById(questionRequest.getTestId()).orElseThrow(
                 () -> new NotFoundException("Test not found"));
 
-        if(questionRequest.getDuration().equals(0) || questionRequest.getDuration() == null) {
+        if (questionRequest.getDuration().equals(0) || questionRequest.getDuration() == null) {
             throw new BadRequestException("The duration shouldn't be zero or null");
         }
-        if(questionRequest.getQuestionType().equals(QuestionType.SELECT_THE_REAL_ENGLISH_WORDS) &&
+        if (questionRequest.getQuestionType().equals(QuestionType.SELECT_THE_REAL_ENGLISH_WORDS) &&
                 questionRequest.getOptions().isEmpty() & questionRequest.getOptions() == null ||
                 questionRequest.getQuestionType().equals(QuestionType.LISTEN_AND_SELECT_ENGLISH_WORDS) &&
                         questionRequest.getOptions().isEmpty() & questionRequest.getOptions() == null ||
@@ -40,7 +41,7 @@ public class QuestionService {
                         questionRequest.getOptions().isEmpty() & questionRequest.getOptions() == null) {
             throw new BadRequestException("You should at least one option!");
         }
-        if(questionRequest.getQuestionType().equals(QuestionType.SELECT_THE_REAL_ENGLISH_WORDS) &&
+        if (questionRequest.getQuestionType().equals(QuestionType.SELECT_THE_REAL_ENGLISH_WORDS) &&
                 questionRequest.getContentRequest().getContentFormat() != ContentFormat.TEXT ||
                 questionRequest.getQuestionType().equals(QuestionType.SELECT_THE_MAIN_IDEA) &&
                         questionRequest.getContentRequest().getContentFormat() != ContentFormat.TEXT ||
@@ -52,46 +53,46 @@ public class QuestionService {
                         questionRequest.getContentRequest().getContentFormat() != ContentFormat.TEXT) {
             throw new BadRequestException("Content format should be <TEXT>!");
         }
-        if(questionRequest.getQuestionType().equals(QuestionType.LISTEN_AND_SELECT_ENGLISH_WORDS) &&
+        if (questionRequest.getQuestionType().equals(QuestionType.LISTEN_AND_SELECT_ENGLISH_WORDS) &&
                 questionRequest.getContentRequest().getContentFormat() != ContentFormat.AUDIO ||
                 questionRequest.getQuestionType().equals(QuestionType.TYPE_WHAT_YOU_HEAR) &&
                         questionRequest.getContentRequest().getContentFormat() != ContentFormat.AUDIO) {
             throw new BadRequestException("Content format should be <AUDIO>!");
-        } else if(questionRequest.getQuestionType().equals(QuestionType.RECORD_SAYING_STATEMENT) &&
+        } else if (questionRequest.getQuestionType().equals(QuestionType.RECORD_SAYING_STATEMENT) &&
                 questionRequest.getContentRequest().getContentFormat() != ContentFormat.RECORD) {
             throw new BadRequestException("Content format should be <RECORD>!");
-        } else if(questionRequest.getQuestionType().equals(QuestionType.DESCRIBE_IMAGE) &&
+        } else if (questionRequest.getQuestionType().equals(QuestionType.DESCRIBE_IMAGE) &&
                 questionRequest.getContentRequest().getContentFormat() != ContentFormat.IMAGE) {
             throw new BadRequestException("Content format should be <IMAGE>!");
         }
-        if(questionRequest.getQuestionType().equals(QuestionType.SELECT_THE_REAL_ENGLISH_WORDS) &&
-                questionRequest.getContentRequest().getContentFormat().equals(ContentFormat.TEXT) &&
-                questionRequest.getOptions() != null & !(questionRequest.getOptions().isEmpty())) {
+        if (questionRequest.getQuestionType().equals(QuestionType.SELECT_THE_REAL_ENGLISH_WORDS) ||
+                questionRequest.getQuestionType().equals(QuestionType.LISTEN_AND_SELECT_ENGLISH_WORDS) &&
+                        questionRequest.getContentRequest().getContentFormat().equals(ContentFormat.TEXT) &&
+                        questionRequest.getOptions() != null & !(questionRequest.getOptions().isEmpty())) {
             int counter = 0;
-            for(OptionRequest option : questionRequest.getOptions()) {
-                if(option.getIsTrue().equals(true)) {
+            for (OptionRequest option : questionRequest.getOptions()) {
+                if (option.getIsTrue().equals(true)) {
                     counter++;
                 }
+                if (option.getOption().isEmpty() || option.getOption() == null) {
+                    throw new BadRequestException("The option shouldn't be empty!");
+                }
             }
-            if(counter <= 1) {
-                throw new BadRequestException("The number of correct answer must be greater 1 or 0");
+            if (counter > 1) {
+                if (questionRequest.getQuestionType().equals(QuestionType.SELECT_THE_REAL_ENGLISH_WORDS)) {
+                    Question question = questionRepository.save(new Question(questionRequest, 1));
+                    question.setTest(test);
+                    question.setOptionType(OptionType.MULTIPLE);
+                } else if (questionRequest.getQuestionType().equals(QuestionType.LISTEN_AND_SELECT_ENGLISH_WORDS)) {
+                    Question question = questionRepository.save(new Question(questionRequest, 2));
+                    question.setTest(test);
+                    question.setOptionType(OptionType.MULTIPLE);
+                }
+                return new SimpleResponse("Question saves successfully!");
+            } else {
+                throw new BadRequestException("You should add more correct answers");
             }
         }
-//        if(questionRequest.getQuestionType().equals(QuestionType.SELECT_THE_REAL_ENGLISH_WORDS) &&
-//                questionRequest.getContentRequest().getContentFormat().equals(ContentFormat.TEXT) &&
-//                questionRequest.getOptions() != null) {
-//            Question question = new Question(questionRequest, 1);
-//            questionRepository.save(question);
-//            return new SimpleResponse("Question saved successfully!");
-//        }
-//        if(questionRequest.getQuestionType().equals(QuestionType.LISTEN_AND_SELECT_ENGLISH_WORDS) &&
-//                questionRequest.getContentRequest().getContentFormat().equals(ContentFormat.AUDIO) &&
-//                questionRequest.getOptions() != null) {
-//            Question question = new Question(questionRequest, 2);
-//            questionRepository.save(question);
-//            return new SimpleResponse("Question saved successfully");
-//        }
-
         return new SimpleResponse("Question save successfully");
     }
 
