e8734b26e8e15a98541e1165a483835f72c1b6b9
User <mavlyan.sadirov@gmail.com>
2023-01-15 16:38:20 +0600

    Question service

--- src/main/java/com/example/bilingual/db/service/QuestionService.java
+++ src/main/java/com/example/bilingual/db/service/QuestionService.java
@@ -29,19 +29,42 @@ public class QuestionService {
             throw new BadRequestException("The duration shouldn't be zero or null");
         }
         if(questionRequest.getQuestionType().equals(QuestionType.SELECT_THE_REAL_ENGLISH_WORDS) &&
-                questionRequest.getContentRequest().getContentFormat().equals(ContentFormat.TEXT) &&
-                questionRequest.getOptions() != null) {
-            Question question = new Question(questionRequest, 1);
-            questionRepository.save(question);
-            return new SimpleResponse("Question saved successfully!");
+                questionRequest.getOptions().isEmpty() & questionRequest.getOptions() == null ||
+                questionRequest.getQuestionType().equals(QuestionType.LISTEN_AND_SELECT_ENGLISH_WORDS) &&
+                        questionRequest.getOptions().isEmpty() & questionRequest.getOptions() == null ||
+                questionRequest.getQuestionType().equals(QuestionType.SELECT_THE_BEST_TITLE) &&
+                        questionRequest.getOptions().isEmpty() & questionRequest.getOptions() == null ||
+                questionRequest.getQuestionType().equals(QuestionType.SELECT_THE_MAIN_IDEA) &&
+                        questionRequest.getOptions().isEmpty() & questionRequest.getOptions() == null) {
+            throw new BadRequestException("You should at least one option!");
         }
-        if(questionRequest.getQuestionType().equals(QuestionType.LISTEN_AND_SELECT_ENGLISH_WORDS) &&
-                questionRequest.getContentRequest().getContentFormat().equals(ContentFormat.AUDIO) &&
-                questionRequest.getOptions() != null) {
-            Question question = new Question(questionRequest, 2);
-            questionRepository.save(question);
-            return new SimpleResponse("Question saved successfully");
+        if(questionRequest.getQuestionType().equals(QuestionType.SELECT_THE_REAL_ENGLISH_WORDS) &&
+                questionRequest.getContentRequest().getContentFormat() != ContentFormat.TEXT ||
+                questionRequest.getQuestionType().equals(QuestionType.SELECT_THE_MAIN_IDEA) &&
+                        questionRequest.getContentRequest().getContentFormat() != ContentFormat.TEXT ||
+                questionRequest.getQuestionType().equals(QuestionType.SELECT_THE_BEST_TITLE) &&
+                        questionRequest.getContentRequest().getContentFormat() != ContentFormat.TEXT ||
+                questionRequest.getQuestionType().equals(QuestionType.HIGHLIGHT_THE_ANSWER) &&
+                        questionRequest.getContentRequest().getContentFormat() != ContentFormat.TEXT ||
+                questionRequest.getQuestionType().equals(QuestionType.RESPOND_IN_AT_LEAST_50_WORDS) &&
+                        questionRequest.getContentRequest().getContentFormat() != ContentFormat.TEXT) {
+            throw new BadRequestException("Content format should be <TEXT>!");
         }
+//        if(questionRequest.getQuestionType().equals(QuestionType.SELECT_THE_REAL_ENGLISH_WORDS) &&
+//                questionRequest.getContentRequest().getContentFormat().equals(ContentFormat.TEXT) &&
+//                questionRequest.getOptions() != null) {
+//            Question question = new Question(questionRequest, 1);
+//            questionRepository.save(question);
+//            return new SimpleResponse("Question saved successfully!");
+//        }
+//        if(questionRequest.getQuestionType().equals(QuestionType.LISTEN_AND_SELECT_ENGLISH_WORDS) &&
+//                questionRequest.getContentRequest().getContentFormat().equals(ContentFormat.AUDIO) &&
+//                questionRequest.getOptions() != null) {
+//            Question question = new Question(questionRequest, 2);
+//            questionRepository.save(question);
+//            return new SimpleResponse("Question saved successfully");
+//        }
+
         return new SimpleResponse("Question save successfully");
     }
 
