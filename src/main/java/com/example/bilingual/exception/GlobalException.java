5d2f47862e3df676139cf3f02811383ce452ed1e
User <mavlyan.sadirov@gmail.com>
2023-01-15 18:17:33 +0600

    Question service

--- src/main/java/com/example/bilingual/exception/GlobalException.java
+++ src/main/java/com/example/bilingual/exception/GlobalException.java
@@ -3,9 +3,9 @@ package com.example.bilingual.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestController
+@RestControllerAdvice
 public class GlobalException {
 
     @ExceptionHandler(NotFoundException.class)
