6d7d272cda9b78085840c6e19113085ac3b273fc
User <mavlyan.sadirov@gmail.com>
2023-02-05 12:54:03 +0600

    Pass test

--- src/main/java/com/example/bilingual/db/service/PassTestService.java
+++ src/main/java/com/example/bilingual/db/service/PassTestService.java
@@ -7,12 +7,15 @@ import com.example.bilingual.db.repository.*;
 import com.example.bilingual.dto.request.PassTestRequest;
 import com.example.bilingual.dto.request.QuestionAnswerRequest;
 import com.example.bilingual.dto.response.SimpleResponse;
+import com.example.bilingual.exception.BadRequestException;
 import com.example.bilingual.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.security.Principal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -35,24 +38,23 @@ public class PassTestService {
         result.setTest(test);
         result.setDateOfSubmission(LocalDateTime.now());
         result.setStatus(Status.NOT_EVALUATED);
+        List<QuestionAnswer> answers = new ArrayList<>();
         for (QuestionAnswerRequest questions : testRequest.getQuestions()) {
             Question question = questionRepository.findById(questions.getQuestionId()).
                     orElseThrow(() -> new NotFoundException("Question not found"));
             QuestionAnswer answer = new QuestionAnswer();
-            if (question.getQuestionType().equals(QuestionType.SELECT_THE_REAL_ENGLISH_WORDS)) {
-                for (Long id : questions.getOptions()) {
-                    Option option = optionRepository.findById(id).orElseThrow(
-                            () -> new NotFoundException("Option not found"));
-                    answer.getOptions().add(option);
-                    option.getQuestionAnswer().add(answer);
+            if (question.getQuestionType().equals(QuestionType.SELECT_THE_REAL_ENGLISH_WORDS) ||
+                    question.getQuestionType().equals(QuestionType.LISTEN_AND_SELECT_ENGLISH_WORDS) ||
+                    question.getQuestionType().equals(QuestionType.SELECT_THE_BEST_TITLE) ||
+                    question.getQuestionType().equals(QuestionType.SELECT_THE_MAIN_IDEA)) {
+                if (question.getQuestionType().equals(QuestionType.SELECT_THE_REAL_ENGLISH_WORDS) ||
+                        question.getQuestionType().equals(QuestionType.LISTEN_AND_SELECT_ENGLISH_WORDS)) {
+                    if(questions.getOptions().isEmpty() || question.getOptions() == null) {
+                        throw new BadRequestException("Choose at least one option");
+                    }
                 }
-                answer.setContent(question.getContent());
-                answer.setQuestion(question);
-                answer.setResult(result);
             }
         }
-
-
         return null;
     }
 }
