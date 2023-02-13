c9b2148f956e72643ae492b68a0edd337c37a4cf
User <mavlyan.sadirov@gmail.com>
2023-02-13 21:34:22 +0600

    Result API

--- src/main/java/com/example/bilingual/api/ResultApi.java
+++ src/main/java/com/example/bilingual/api/ResultApi.java
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
