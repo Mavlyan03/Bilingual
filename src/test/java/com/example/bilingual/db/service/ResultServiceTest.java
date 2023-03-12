5555177d66e45a93925a80d40b9587c196b72d86
User <mavlyan.sadirov@gmail.com>
2023-03-12 10:22:06 +0600

    Junit test

--- src/test/java/com/example/bilingual/db/service/ResultServiceTest.java
+++ src/test/java/com/example/bilingual/db/service/ResultServiceTest.java
@@ -1,12 +1,19 @@
 package com.example.bilingual.db.service;
 
+import com.example.bilingual.db.model.Result;
 import com.example.bilingual.db.repository.ResultRepository;
+import com.example.bilingual.dto.response.ResultResponse;
+import com.example.bilingual.dto.response.ViewResultResponse;
+import com.example.bilingual.exception.NotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.transaction.Transactional;
 
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -21,6 +28,10 @@ class ResultServiceTest {
 
     @Test
     void getAllResults() {
+        List<ResultResponse> results = resultService.getAllResults();
+
+        assertNotNull(results);
+        assertEquals(results.size(), 1);
     }
 
     @Test
@@ -41,14 +52,25 @@ class ResultServiceTest {
 
     @Test
     void deleteResult() {
-    }
+        List<ResultResponse> result = resultService.deleteResult(1L);
 
-    @Test
-    void delete() {
+        assertNotNull(result);
+        assertThatThrownBy(() -> resultService.getResultById(1L))
+                .isInstanceOf(NotFoundException.class)
+                .hasMessageContaining("Result not found");
     }
 
     @Test
     void getResultById() {
+        Result result1 = resultRepository.findById(1L).orElseThrow();
+        ViewResultResponse result = resultService.getResultById(1L);
+
+        assertNotNull(result);
+        assertEquals(result.getId(), result1.getId());
+        assertEquals(result.getTest(), result1.getTest().getTitle());
+        assertEquals(result.getFinalScore(), result1.getScore());
+        assertEquals(result.getFinalStatus(), result1.getStatus());
+        assertEquals(result.getDateOfSubmission(), result1.getDateOfSubmission());
     }
 
     @Test
